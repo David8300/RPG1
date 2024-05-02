328c1dcc0d60078be6b5339a73cc6f4552a45aad
David8300 <131800990+David8300@users.noreply.github.com>
2024-05-02 18:36:11 -0400

    Start
    Importing files

---
+++ youtube/script.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@onready var player=$".."
+@onready var animation=$AnimatedSprite2D
+const SPEED = 150.0
+
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("IZQUIERDA"):
+		player.position.x -= SPEED*delta
+		animation.play("izquierda")
+	if Input.is_action_pressed("DERECHA"):
+		player.position.x += SPEED*delta
+		animation.play("derecha")
+	if Input.is_action_pressed("ARRIBA"):
+		player.position.y -= SPEED*delta
+		animation.play("arriba")
+	if Input.is_action_pressed("ABAJO"):
+		player.position.y += SPEED*delta
+		animation.play("abajo")
+	else:
+		animation.stop()
+	move_and_slide()
